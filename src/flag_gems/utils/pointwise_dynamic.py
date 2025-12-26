d0efbf1a60e0c6b096115b87dc65a558697a845c
v2yield <wye2207256537@gmail.com>
2025-12-26 18:32:59 +0800

    optimize mm bf16/fp16

--- src/flag_gems/utils/pointwise_dynamic.py
+++ src/flag_gems/utils/pointwise_dynamic.py
@@ -6,6 +6,7 @@ import torch
 import triton
 from triton.runtime.jit import JITFunction
 
+from flag_gems.runtime import torch_device_fn
 from flag_gems.utils.code_cache import code_cache_dir
 from flag_gems.utils.code_utils import IndentedBuffer, write_atomic
 from flag_gems.utils.codegen_config_utils import CodeGenConfig, get_codegen_config
@@ -822,15 +823,25 @@ class WrapperGenerator:
             with code.indent():
                 self.gen_return(code)
             max_tile_size = self.config.max_tile_size
-            code.writeline(
-                f"tile_sizes = heuristics_for_tile_size({max_tile_size}, *shape)"
+            capability = torch_device_fn.get_device_capability(
+                torch_device_fn.current_device()
             )
+            if self.name.find("fill_scalar") != -1 and capability[0] >= 9:
+                code.writeline("tile_sizes = tuple([64])")
+            else:
+                code.writeline(
+                    f"tile_sizes = heuristics_for_tile_size({max_tile_size}, *shape)"
+                )
             code.writeline("tile_size = math.prod(tile_sizes)")
             code.writeline(
                 "num_tiles = math.prod(triton.cdiv(size, tile_size) for size, tile_size in zip(shape, tile_sizes))"
             )
-            max_grid_size0 = self.config.max_grid_size[0]
-            code.writeline(f"num_ctas = min({max_grid_size0}, num_tiles)")
+
+            if capability[0] >= 9:
+                code.writeline("num_ctas = num_tiles")
+            else:
+                max_grid_size0 = self.config.max_grid_size[0]
+                code.writeline(f"num_ctas = min({max_grid_size0}, num_tiles)")
 
             code.writeline("tiles_per_cta = triton.cdiv(num_tiles, num_ctas)")
             code.writeline("num_warps = heuristics_for_num_warps(tile_size)")
